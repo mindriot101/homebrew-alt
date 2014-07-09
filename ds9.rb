8aa819d5b4e042007dde894b9f313f4f9c616e01
Simon Walker <s.r.walker101@googlemail.com>
2014-07-09 11:27:03 +0100

    Splits ds9 install based on OS version

--- ds9.rb
+++ ds9.rb
@@ -2,8 +2,17 @@ require 'formula'
 
 class Ds9 < Formula
   homepage 'http://hea-www.harvard.edu/RD/ds9/site/Home.html'
-  url 'http://hea-www.harvard.edu/RD/ds9/download/darwinmountainlion/ds9.darwinmountainlion.7.2.tar.gz'
-  sha1 '4d7d17fd619ef5aa2fc14618811207faad797d76'
+
+  if MacOS.version >= :mountain_lion
+    url 'http://hea-www.harvard.edu/RD/ds9/download/darwinmountainlion/ds9.darwinmountainlion.7.2.tar.gz'
+    sha1 '4d7d17fd619ef5aa2fc14618811207faad797d76'
+  elsif MacOS.version == :lion
+    url 'http://ds9.si.edu/download/darwinlion/ds9.darwinlion.7.2.tar.gz'
+    sha1 '8500b3ec15c91c83e126dfb1857e0b13b0e7ab9e'
+  else
+    url 'http://ds9.si.edu/download/darwinsnowleopard/ds9.darwinsnowleopard.7.2.tar.gz'
+    sha1 'b6a1ef79812f8a7d2bbccd9e3e5640d1e60b2fa7'
+  end
 
   depends_on :x11
 
