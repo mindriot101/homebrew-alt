2b38698bf71177eed90707900ef4a2267db9d093
Simon Walker <s.r.walker101@googlemail.com>
2014-12-11 15:53:52 +0000

    Add jktebop formula

---
+++ jktebop.rb
@@ -0,0 +1,25 @@
+require "formula"
+
+class Jktebop < Formula
+  homepage "http://www.astro.keele.ac.uk/jkt/codes/jktebop.html"
+  url "http://www.astro.keele.ac.uk/jkt/codes/jktebop-v34.tgz"
+  sha1 "ea08d44eafb337e7ad0aab06197614ffd209099c"
+
+  option "with-examples", "Install the examples into share/#{name}"
+
+  depends_on :fortran
+
+  def install
+    system "#{ENV.fc} -O2 -o #{name} #{name}.f"
+    bin.install name
+
+    if build.with? "examples"
+      mkdir "#{name}" do
+        %w{dat out fit in par}.each do |ext|
+          mv Dir.glob("../*.#{ext}"), '.'
+        end
+      end
+      share.install "#{name}"
+    end
+  end
+end
